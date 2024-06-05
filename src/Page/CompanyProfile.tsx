f9c0e0c23ff15b7c8a593257df35c19e6fc47f13
Vikas Sharma <you@example.com>
2024-06-06 00:03:39 +0530

    first commit

---
+++ src/Page/CompanyProfile.tsx
@@ -0,0 +1,24 @@
+import  { useState } from "react";
+
+function CompanyProfile() {
+
+  return (
+    <div className="bg-neutral-900 h-full">
+      <div className="grid gap-4 grid-cols-6 p-4">
+        <div className="bg-neutral-800 h-12 col-span-3 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-3 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-3 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-3 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-2 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-2 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-2 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-3 bg-white rounded-lg mb-8"></div>
+        <div className="bg-neutral-800 h-12 col-span-3 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-3 bg-white rounded-lg"></div>
+        <div className="bg-neutral-800 h-12 col-span-3 bg-white rounded-lg"></div>
+      </div>
+    </div>
+  );
+}
+
+export default CompanyProfile;
