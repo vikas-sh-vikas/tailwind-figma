847286682697b42849e56da58d4d21aeed4f7644
Vikas Sharma <you@example.com>
2024-06-06 23:16:29 +0530

    Modal Edit

--- src/Component/Modal.tsx
+++ src/Component/Modal.tsx
@@ -1,29 +1,42 @@
-import React from 'react'
+import React from "react";
 type ModalProps = {
-    isOpen:boolean
-    setIsModal: () => void
-}
+  isOpen: boolean;
+  setIsModal: () => void;
+};
 
-function Modal(props:ModalProps) {
-    const modalStatus = props.isOpen
-    const CloseModal = (e:any) => {
-        if(e.target.id == "modal")
-            props.setIsModal()
-    }
-    if(props.isOpen)
-        return null
-    else
-     return (
-    <div id="modal" className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' onClick={CloseModal}>
-        <div className='bg-white bg-opacity-25 h-1/3 w-1/3'>
-            <form>
-                <input></input>
-            <button onClick={()=>props.setIsModal()}>Close</button>
-            </form>
-            <button onClick={()=>props.setIsModal()}>Close</button>
+function Modal(props: ModalProps) {
+  const modalStatus = props.isOpen;
+  const CloseModal = (e: any) => {
+    if (e.target.id == "modal") props.setIsModal();
+  };
+  if (props.isOpen) return null;
+  else
+    return (
+      <div
+        id="modal"
+        className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+        onClick={CloseModal}
+      >
+        <div className="bg-white bg-opacity-25 min-h-1/3 w-1/3">
+          <span className="text-end p-2" onClick={() => props.setIsModal()}>
+            X
+          </span>
+          <form className=" p-4">
+            <h1 className="text-xl text-white text-end">تعديل اسم الشركة</h1>
+            <div className="flex flex-col">
+              <label className="pb-2">الاسم الجديد</label>
+              <input
+                className="mb-2"
+                placeholder="ادخل اسمالشركة الجديد..."
+              ></input>
+              <button type="submit" className="bg-orange-600">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-    </div>
-  )
+      </div>
+    );
 }
 
-export default Modal
+export default Modal;
