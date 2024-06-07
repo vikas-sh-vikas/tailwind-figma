9c865ca6c7f3fda1f96c1ab6cf67a337fd30f418
tdspl-vikas <vikas.sharma@techondater.co.in>
2024-06-07 14:14:51 +0530

    Dynamic Components

--- src/Component/Modal.tsx
+++ src/Component/Modal.tsx
@@ -1,36 +1,57 @@
 import React from "react";
 type ModalProps = {
   isOpen: boolean;
-  setIsModal: () => void;
+  data: {
+    label: string;
+    input: string;
+  };
+  closeModal: () => void;
+  setData: (data: string) => void;
 };
 
-function Modal(props: ModalProps) {
-  const modalStatus = props.isOpen;
-  const CloseModal = (e: any) => {
-    if (e.target.id == "modal") props.setIsModal();
+function Modal({ isOpen, data, closeModal, setData }: ModalProps) {
+  const submitFun = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submit");
+    closeModal();
   };
-  if (props.isOpen) return null;
+  const setDataInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData(e.target.value);
+  };
+  const CloseModal = (e: any) => {
+    if (e.target.id == "modal") closeModal();
+  };
+  if (isOpen == false) return null;
   else
     return (
       <div
         id="modal"
-        className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+        className="z-10 index-1 fixed inset-0 bg-black bg-opacity-25 backdrop-blur-lg flex justify-center items-center"
         onClick={CloseModal}
       >
-        <div className="bg-white bg-opacity-25 min-h-1/3 w-1/3">
-          <span className="text-end p-2" onClick={() => props.setIsModal()}>
-            X
-          </span>
-          <form className=" p-4">
-            <h1 className="text-xl text-white text-end">تعديل اسم الشركة</h1>
+        <div className="bg-neutral-600 bg-opacity-25 min-h-1/3 w-1/3">
+          <form className=" p-8" onSubmit={submitFun}>
+            <h1 className="text-xl text-white text-center">تعديل اسم الشركة</h1>
             <div className="flex flex-col">
-              <label className="pb-2">الاسم الجديد</label>
+              <label className="pb-2 text-end text-neutral-400">
+                {data.label}
+              </label>
+
               <input
-                className="mb-2"
-                placeholder="ادخل اسمالشركة الجديد..."
-              ></input>
-              <button type="submit" className="bg-orange-600">
-                Submit
+                type="text"
+                id="company"
+                value={data.input}
+                className="mb-8 text-end bg-neutral-400 bg-opacity-25 border-neutral-900 text-neutral-400 text-sm rounded-xl w-full p-3.5"
+                placeholder={`${data.label}...`}
+                onChange={setDataInput}
+                required
+              />
+
+              <button
+                type="submit"
+                className="bg-gradient-to-r from-orange-500 to-orange-300 hover:from-orange-400  hover:to-orange-200 text-white rounded-3xl py-3"
+              >
+                حفظ التعديل
               </button>
             </div>
           </form>
--- src/Page/CompanyProfile.tsx
+++ src/Page/CompanyProfile.tsx
@@ -1,51 +1,56 @@
-import  { useState } from "react";
+import { useState } from "react";
 import Modal from "../Component/Modal";
+import EditComponent from "../Component/EditComponent";
 
 function CompanyProfile() {
-  const [isModal, setIsModal] = useState(true)
+  const [isModal, setIsModal] = useState(true);
 
   return (
-    <div className="bg-neutral-900 h-hvh">
-      <button className="bg-blue-600 p-2" onClick={()=>setIsModal(!isModal)}>Modal</button>
-      <Modal isOpen={isModal} setIsModal={()=>setIsModal(!isModal)}/>
+    <div className="h-screen bg-neutral-900">
       <div className="grid gap-2 grid-cols-6 p-4">
         <h1 className="col-span-6 text-white text-end">ملف الشركة</h1>
-        <div className="bg-neutral-700 col-span-3  rounded-lg p-2">
+        <EditComponent span={3} 
+        data={{
+        label:"موقع الشركة",
+        input:", شارع14 بجوار محطة المترو",
+        }}/>
+        <EditComponent span={3} 
+        data={{
+        label:"الاسم",
+        input:"الشهد للمقاولات العمومية",
+        }}/>
+        <EditComponent span={3} 
+        data={{
+        label:"تاريخ بداية العمل",
+        input:"14/10/2022",
+        }}/>
+        <EditComponent span={3} 
+        data={{
+        label:"رقم السجل التجارى",
+        input:"1564562951544984",
+        }}/>
+        <div className="bg-neutral-700 h-full col-span-2  rounded-lg p-2 mb-4">
           <p className="text-sm text-neutral-400 text-end">موقع الشركة</p>
-          <p className="text-lg text-white text-end">الرياض, شارع14 بجوار محطة المترو</p>
+          <p className="text-lg text-white text-end">
+            الرياض, شارع14 بجوار محطة المترو
+          </p>
         </div>
-        <div className="bg-neutral-700 col-span-3  rounded-lg p-2">
-          <p className="text-sm text-neutral-400 text-end">موقع الشركة</p>
-          <p className="text-lg text-white text-end">الرياض, شارع14 بجوار محطة المترو</p>
-        </div>
-        <div className="bg-neutral-700 col-span-3  rounded-lg p-2">
-          <p className="text-sm text-neutral-400 text-end">موقع الشركة</p>
-          <p className="text-lg text-white text-end">الرياض, شارع14 بجوار محطة المترو</p>
-        </div>
-        <div className="bg-neutral-700 col-span-3  rounded-lg p-2">
-          <p className="text-sm text-neutral-400 text-end">موقع الشركة</p>
-          <p className="text-lg text-white text-end">الرياض, شارع14 بجوار محطة المترو</p>
-        </div>
-        <div className="bg-neutral-700 col-span-2  rounded-lg p-2 mb-4">
-          <p className="text-sm text-neutral-400 text-end">موقع الشركة</p>
-          <p className="text-lg text-white text-end">الرياض, شارع14 بجوار محطة المترو</p>
-        </div>
-        <div className="bg-neutral-700 col-span-2  rounded-lg p-2 mb-4">
-          <p className="text-sm text-neutral-400 text-end">موقع الشركة</p>
-          <p className="text-lg text-white text-end">الرياض, شارع14 بجوار محطة المترو</p>
-        </div>
-        <div className="bg-neutral-700 col-span-2  rounded-lg p-2 mb-4">
-          <p className="text-sm text-neutral-400 text-end">موقع الشركة</p>
-          <p className="text-lg text-white text-end">الرياض, شارع14 بجوار محطة المترو</p>
-        </div>
-
-
+        <EditComponent span={2} 
+        data={{
+        label:"رقم الهاتف",
+        input:"+201092124608",
+        }}/>
+        <EditComponent span={2} 
+        data={{
+        label:"مجال العمل",
+        input:"العقارات والطرق",
+        }}/>
+        {/* <div className="bg-neutral-700 h-12 col-span-3 rounded-lg"></div>
         <div className="bg-neutral-700 h-12 col-span-3 rounded-lg"></div>
         <div className="bg-neutral-700 h-12 col-span-3 rounded-lg"></div>
-        <div className="bg-neutral-700 h-12 col-span-3 rounded-lg"></div>
-        <div className="bg-neutral-700 h-12 col-span-3 rounded-lg"></div>
+        <div className="bg-neutral-700 h-12 col-span-3 rounded-lg"></div> */}
       </div>
-    </div> 
+    </div>
   );
 }
 
